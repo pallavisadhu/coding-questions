d4af0f04d748d479ab440c4f2c278e71e4725d68
pallavisadhu <pallavisadhu@yahoo.com>
2022-12-03 18:59:26 -0600

    commited first coding exercise 1

---
+++ CodingAssignment/src/test/java/Assignment2/CodingAssignment/CodingAssignment1.java
@@ -0,0 +1,18 @@
+package Assignment2.CodingAssignment;
+
+import org.testng.annotations.Test;
+
+public class CodingAssignment1 {
+	
+	@Test
+	public void printHello() {
+		System.out.println("Hello" +" "+"AHCECLWLXO");
+
+	}
+	
+	@Test
+	public void helloUpper() {
+		System.out.println("HELLO" + " " + "ahceclwlxo");
+	}	
+
+}
